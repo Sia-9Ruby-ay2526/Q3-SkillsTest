fd7661c2e0ab779174c5514020e5cdf371a83dde
Maria Agatha Sia <sia.mb@obmontessori.edu.ph>
2026-03-13 13:55:40 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let total = 250; // Base fare in pesos
+let subtotal = 250 * 3
+
+//variables
+function getTotalAndSubtotal() {
+    const ADMEEN_FI = 50.00;
+    const TEECKET_PRESYO = 250;
+
+    let subtotal =  ADMEEN_FI*TEECKET_PRESYO ;
+    let total = ADMEEN_FI + TEECKET_PRESYO ;
+    let quantity = document.getElementById("total").value;
+}
+    
