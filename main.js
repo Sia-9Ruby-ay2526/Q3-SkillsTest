3184689984148199a90c09757c2b99ea938315f3
Maria Agatha Sia <sia.mb@obmontessori.edu.ph>
2026-03-15 10:27:49 +0800

    Refactor getTotal function to remove parameter

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-function getTotal(result_ticket) {
+function getTotal() {
 
     const TICKET_PRICE = 250;
     const ADMIN_FEE = 50.00;
 
-    let ticket = document.getElementById("get_your_ticket").value;
+    let ticket = Number(document.getElementById("get_your_ticket").value);
     let subtotal = TICKET_PRICE * ticket;
     let total = subtotal + ADMIN_FEE;
 
     document.getElementById("subtotal").innerHTML = "₱" + subtotal.toFixed(2);
-    document.getElementById("total").innerHTML = "₱" + total.toFixed(2);
-}
-    
+    document.getElementById("totalprice").innerHTML = "₱" + total.toFixed(2);
+
+}  
